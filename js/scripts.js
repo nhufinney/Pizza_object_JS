0686ce18a2dda22efe18a135eb1cfa64dd49e90b
nhu finney <nhufinney@gmail.com>
2015-04-17 10:12:07 -0700

    finish html files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Create Pizza prototype
 //Pizza has 3 types: small, medium, large === size 1, 2, 3
 //Pizza cost has 2 prices: cheese ($5) and pepperoni($7)
 var Pizza = {
@@ -20,3 +21,22 @@ var Pizza = {
         this.pizza_price = this.pizza_size * this.pizza_cost;
     }
 };
+
+//Create an customer object initial from Pizza prototype
+var customer = Object.create(Pizza);
+
+//JS code to run the page
+$(document).ready(function() {
+ $("form#pizza").submit(function(event) {
+     event.preventDefault();
+
+     var input_size = parseInt($("select#size").val());
+     var input_type = parseInt($("select#type").val());
+
+     customer.size(input_size);
+     customer.cost(input_type);
+     customer.price();
+
+     $(".show_cost").text(customer.pizza_price);
+ });
+});
