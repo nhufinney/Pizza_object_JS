ddee44fde24b510204ac068763cb27bf279b722d
nhu finney <nhufinney@gmail.com>
2015-04-17 09:35:57 -0700

    pass the second test to set pizza type.

--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,15 @@ describe('Pizza', function(){
             expect(testPizza.pizza_size).to.equal(3);
         });
     });
+
+    describe("type", function(){
+        it ("set the type of pizza into the property pizza_type", function(){
+            var testPizza = Object.create(Pizza);
+            testPizza.type(2);
+            expect(testPizza.pizza_type).to.equal(1.2);
+        });
+    });
+
+
+
 });
